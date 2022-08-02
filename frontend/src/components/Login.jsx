a0a96e4aa6050e51bd21ff2fb0d99b43e226397c
Hasegawa-Akito <h.akito1999@gmail.com>
2022-08-02 22:46:30 +0900

    api処理準備

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,13 +1,22 @@
 import { auth } from '../firebase';
 import { useEffect } from 'react';
 import LoginButton from './LoginButton';
+import axios from "axios";
 
 function Login() {
     useEffect(() => {
-    
+        // バックエンドでのユーザー登録
+        const login_confirm = async () => {
+            const response = await axios.get('https://api.github.com/users/tagty');
+            console.log(response.data)
+        }
+          
         // 認証チェック処理
         auth.onAuthStateChanged(user => {
-            console.log('認証チェック', user);
+            if (user) {
+                login_confirm()
+            }
+            
         });
 
     },[auth]);
