2acee47a4a05a6ef07e199c294efbe53768118a1
Hasegawa-Akito <h.akito1999@gmail.com>
2022-08-02 17:15:52 +0900

    修正

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -10,14 +10,6 @@ storageBucket: import.meta.env.VITE_STORAGE_BUCKETY,
 messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
 appId: import.meta.env.VITE_APP_ID
 };
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAtrYxazFwRhrGxvEFGgoH48lEkI5tyKtY",
-//     authDomain: "shamer-4a9aa.firebaseapp.com",
-//     projectId: "shamer-4a9aa",
-//     storageBucket: "shamer-4a9aa.appspot.com",
-//     messagingSenderId: "1014648734822",
-//     appId: "1:1014648734822:web:3da1a4a1de250c39a759a7"
-//   };
 
 // googleのログイン認証
 const app = initializeApp(firebaseConfig);
