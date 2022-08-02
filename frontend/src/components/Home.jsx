af95baea7853ee01c8d8a854f773db0b7135d71d
nisi0929 <kengo279@icloud.com>
2022-08-02 23:13:18 +0900

    sidebarの追加

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
-import reactLogo from "../assets/react.svg";
-import "../App.css";
+import { useState } from 'react';
+import reactLogo from '../assets/react.svg';
+import { Sidebar } from './layouts/Sidebar';
+import '../App.css';
 
 function Home() {
   return (
     <div>
+      <Sidebar />
       <h1>Home</h1>
     </div>
   );
