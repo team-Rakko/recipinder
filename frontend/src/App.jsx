30f9befd057ad9e2871a552ef150b0e51e75ef99
ysdko <koki.yoshida110@gmail.com>
2022-08-03 12:31:19 +0900

    スワイプ画面の雛形作成

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Test from "./components/Test.jsx";
 import Home from "./components/Home.jsx";
 import Login from "./components/Login.jsx";
-import './App.css'
+import Swipe from "./components/Swipe.jsx";
+import "./App.css";
 function App() {
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="test" element={<Test />}></Route>
         <Route path="login" element={<Login />}></Route>
+        <Route path="swipe" element={<Swipe />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/Swipe.jsx
@@ -0,0 +1,132 @@
+import React, { useState, useMemo, useRef } from "react";
+import TinderCard from "react-tinder-card";
+
+const db = [
+  {
+    id: 0,
+    name: "Richard Hendricks",
+    url: "./img/meat.jpg",
+  },
+  {
+    id: 1,
+    name: "Erlich Bachman",
+    url: "./img/fish.jpg",
+  },
+  {
+    id: 2,
+    name: "Monica Hall",
+    url: "./img/vegetable.jpg",
+  },
+];
+
+function Swipe() {
+  const [currentIndex, setCurrentIndex] = useState(db.length - 1);
+  const [lastDirection, setLastDirection] = useState();
+  const currentIndexRef = useRef(currentIndex);
+
+  const childRefs = useMemo(
+    () =>
+      Array(db.length)
+        .fill(0)
+        .map((i) => React.createRef()),
+    []
+  );
+
+  const updateCurrentIndex = (val) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
+  };
+
+  const canGoBack = currentIndex < db.length - 1;
+
+  const canSwipe = currentIndex >= 0;
+
+  // set last direction and decrease current index
+  const swiped = (direction, nameToDelete, index) => {
+    setLastDirection(direction);
+    updateCurrentIndex(index - 1);
+  };
+
+  const outOfFrame = (name, idx) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current);
+    // handle the case in which go back is pressed before card goes outOfFrame
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
+    // TODO: when quickly swipe and restore multiple times the same card,
+    // it happens multiple outOfFrame events are queued and the card disappear
+    // during latest swipes. Only the last outOfFrame event should be considered valid
+  };
+
+  const swipe = async (dir) => {
+    if (canSwipe && currentIndex < db.length) {
+      await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
+    }
+  };
+
+  // increase current index and show card
+  const goBack = async () => {
+    if (!canGoBack) return;
+    const newIndex = currentIndex + 1;
+    updateCurrentIndex(newIndex);
+    await childRefs[newIndex].current.restoreCard();
+  };
+
+  return (
+    <div>
+      <div className="cardContainer">
+        {db.map((character, index) => (
+          <div style={{ position: "absolute", left: 20, right: 20, top: 80 }}>
+            <TinderCard
+              ref={childRefs[index]}
+              className="swipe"
+              key={character.name}
+              onSwipe={(dir) => swiped(dir, character.name, index)}
+              onCardLeftScreen={() => outOfFrame(character.name, index)}
+            >
+              <div
+                style={{
+                  backgroundImage: "url(" + character.url + ")",
+                  height: "100px",
+                  backgroundSize: "contain",
+                }}
+                className="card"
+              >
+                <h3>{character.name}</h3>
+              </div>
+            </TinderCard>
+          </div>
+        ))}
+      </div>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+          onClick={() => swipe("left")}
+        >
+          Swipe left!
+        </button>
+        <button
+          style={{ backgroundColor: !canGoBack && "#c3c4d3" }}
+          onClick={() => goBack()}
+        >
+          Undo swipe!
+        </button>
+        <button
+          style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+          onClick={() => swipe("right")}
+        >
+          Swipe right!
+        </button>
+      </div>
+      {lastDirection ? (
+        <h2 key={lastDirection} className="infoText">
+          You swiped {lastDirection}
+        </h2>
+      ) : (
+        <h2 className="infoText">
+          Swipe a card or press a button to get Restore Card button visible!
+        </h2>
+      )}
+    </div>
+  );
+}
+
+export default Swipe;
