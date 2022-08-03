1c854bca0ba4a5959e6247d44fd4bc50fb9aa17f
KeitaOsaki <keita.base.05@gmail.com>
2022-08-03 14:10:41 +0900

    [add]リスト追加用の機能を実装

---
+++ backend/pkg/controller/list.go
@@ -0,0 +1,42 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"recipinder/pkg/model/dao"
+	"recipinder/pkg/model/dto"
+	"recipinder/pkg/view"
+)
+
+func ListMyRecipe() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var lar dto.ListAddRequest
+		if err := c.BindJSON(&lar); err != nil {
+			log.Println(err)
+			view.ReturnErrorResponse(
+				c,
+				http.StatusBadRequest,
+				"Bad Request",
+				"Wrong request body",
+			)
+			return
+		}
+
+		client := dao.MakeAddListClient()
+		err := client.Request(lar)
+		if err != nil {
+			log.Println(err)
+			view.ReturnErrorResponse(
+				c,
+				http.StatusInternalServerError,
+				"Internal Server Error",
+				err.Error(),
+			)
+			return
+		}
+
+		me := "success"
+		c.JSON(http.StatusOK, view.ReturnListResponse(me))
+	}
+}
