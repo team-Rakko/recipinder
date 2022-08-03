bb5b61ec5a39ff261299c4436c287a6c6e5108f4
Hasegawa-Akito <h.akito1999@gmail.com>
2022-08-03 16:30:16 +0900

    buttonのcss変更

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -28,7 +28,7 @@ function Login() {
 
     return (
       <div className="background">
-        <div className="loginButton absolute top-96 left-30 z-10">
+        <div className="absolute top-96 left-30 z-10">
             <LoginButton />
         </div>
 
