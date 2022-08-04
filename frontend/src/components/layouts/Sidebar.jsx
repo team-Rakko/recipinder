9998ca83f6954ce7743f00f1e3aa4c82d4a0e306
nisi0929 <kengo279@icloud.com>
2022-08-04 12:56:22 +0900

    fix header(sidebar)

--- frontend/src/components/layouts/Sidebar.jsx
+++ frontend/src/components/layouts/Sidebar.jsx
@@ -2,16 +2,29 @@ import House from '../../assets/images/house.svg';
 import Search from '../../assets/images/search.svg';
 import List from '../../assets/images/list.svg';
 import { Menu } from './Menu';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../../assets/css/menu.css';
 export const Sidebar = () => {
   const [hover, setHover] = useState(false);
   const [styles, setStyles] = useState('left-10');
+  useEffect(() => {
+    setHover(true);
+    setStyles('left-0 sm:left-20 sm:top-5 top-20 sm:w-3/4 w-full opacity-100');
+    setTimeout(() => {
+      setStyles('left-10 opacity-0 sm:w-3/4 w-full');
+      setTimeout(() => {
+        setHover(false);
+      }, 4000);
+    }, 2000);
+  }, []);
+
   return (
     <div
       onMouseLeave={() => {
-        setHover(false);
-        setStyles('left-10 opacity-0');
+        setTimeout(() => {
+          setHover(false);
+        }, 80);
+        setStyles('left-10 opacity-0 sm:w-3/4 w-full');
       }}
     >
       <div className="bg-white grid sm:grid-cols-1 grid-cols-3 absolute sm:gap-10 gap-10 sm:py-10 py-3 sm:px-3 px-10 place-items-center justify-items-center rounded-lg top-5 left-5 z-20">
@@ -21,12 +34,11 @@ export const Sidebar = () => {
           className="w-8 h-8"
           onMouseEnter={() => {
             setHover(true);
-            console.log('hover');
             setTimeout(() => {
               setStyles(
                 'left-0 sm:left-20 sm:top-5 top-20 sm:w-3/4 w-full opacity-100'
               );
-            }, 20);
+            }, 40);
           }}
         />
         <img src={House} alt="" className="w-8 h-8" />
