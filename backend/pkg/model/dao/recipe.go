d543b9b88de2803a69d696e5319cb4b7e424ae17
KeitaOsaki <keita.base.05@gmail.com>
2022-08-04 21:42:19 +0900

    [fix]レスポンスを修正

--- backend/pkg/model/dao/recipe.go
+++ backend/pkg/model/dao/recipe.go
@@ -10,7 +10,9 @@ import (
 const (
 	//select work
 	SelectRecipeList   = "select recipis.id,recipis.recipe_name,recipis.url from recipis where id > ? and tag = ? limit 10"
-	SelectRecipeDetail = "select recipis.recipe_name,recipis.description,recipis.url,recipis.place,recipis.ingredients,recipis.evaluation from recipis where id = ?"
+	SelectRecipeDetail = "select recipis.recipe_name,recipis.description,recipis.url,recipis.place,recipis.ingredients,recipis.evaluation,recipis.kcal,recipis.time from recipis where id = ?"
+
+	selectRecipeProcess = "select number,explanation from recipe_process where recipe_id = ?"
 )
 
 ///post work
@@ -75,12 +77,40 @@ func (info *readRecipeDetail) Request(recipeInfo dto.RecipeDetailRequest) (dto.R
 	var err error
 	var recipe dto.RecipeDetail
 	row := Conn.QueryRow(SelectRecipeDetail, recipeInfo.Id)
-	if err := row.Scan(&recipe.RecipeName, &recipe.Description, &recipe.Url, &recipe.Place, &recipe.Ingredients, &recipe.Evaluation); err != nil {
+	if err := row.Scan(&recipe.RecipeName, &recipe.Description, &recipe.Url, &recipe.Place, &recipe.Ingredients, &recipe.Evaluation, &recipe.Kcal, &recipe.Time); err != nil {
 		if err == sql.ErrNoRows {
 			return rd, errors.New("")
 		}
 		log.Println(err)
 	}
+
+	var process []dto.Process
+
+	rows, err := Conn.Query(selectRecipeProcess, recipeInfo.Id)
+	if err != nil {
+		log.Println(err)
+		if err == sql.ErrNoRows {
+			return rd, errors.New("not exist data")
+		}
+	}
+	defer rows.Close()
+	for rows.Next() {
+		pro := &dto.Process{}
+		if err := rows.Scan(&pro.Number, &pro.Exp); err != nil {
+			return rd, errors.New("exist nil work data")
+		}
+		process = append(process, *pro)
+	}
+	if len(process) == 0 {
+		return rd, errors.New("recipe list id is wrong")
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Println(err)
+		return rd, errors.New("err")
+	}
+	recipe.Process = process
 
 	return recipe, err
 }
