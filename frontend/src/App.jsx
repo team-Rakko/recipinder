93d4d62223f56d04e2fb05b7ff35ebc3edb3a18f
ysdko <koki.yoshida110@gmail.com>
2022-08-04 22:27:49 +0900

    スワイプ画像の取得と更新

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { createContext, useState, useEffect } from 'react';
-import Routing from './Routing';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createContext, useState, useEffect } from "react";
+import Routing from "./Routing";
+import "./App.css";
 export const UserContext = createContext();
 
 function App() {
   // useContextとuseStateを連携し値を別コンポーネントで変更可能
-  const [userInfo, setUserInfo] = useState({ userName: '', uid: '', login: false, path: "/swipe" });
+  const [userInfo, setUserInfo] = useState({
+    userName: "",
+    uid: "",
+    login: true,
+    path: "/swipe2",
+  });
   const contextValue = { userInfo, setUserInfo };
-
 
   return (
     <UserContext.Provider value={contextValue}>
-
       <BrowserRouter>
         <Routing />
       </BrowserRouter>
-
     </UserContext.Provider>
   );
 }
-
 
 export default App;
