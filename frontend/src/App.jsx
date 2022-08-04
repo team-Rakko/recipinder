3122477d4cb6ee26f7a37a5202ebd016a17d28a4
Hasegawa-Akito <h.akito1999@gmail.com>
2022-08-04 17:51:25 +0900

    ログインしていない時のルーティング処理

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { createContext, useState, useEffect } from 'react';
-import Test from './components/Test.jsx';
-import Home from './components/Home.jsx';
-import Login from './components/Login.jsx';
-import Swipe from './components/Swipe.jsx';
+import Routing from './Routing';
 import './App.css';
-import { Sidebar } from './components/layouts/Sidebar';
 export const UserContext = createContext();
 
 function App() {
   // useContextとuseStateを連携し値を別コンポーネントで変更可能
-  const [userInfo, setUserInfo] = useState({ userName: '', uid: '' });
-  const [onSidebar, setOnSidebar] = useState(false);
+  const [userInfo, setUserInfo] = useState({ userName: '', uid: '', login: false, path: "/swipe" });
   const contextValue = { userInfo, setUserInfo };
 
-  useEffect(() => {
-  
-    if (window.location.pathname != "/login") {
-      setOnSidebar(true);
-    }
-    
-  }, []);
-  
 
   return (
     <UserContext.Provider value={contextValue}>
 
-      {onSidebar && <Sidebar />}
-
       <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/" element={<Home />}></Route>
-          <Route path="test" element={<Test />}></Route>
-          <Route path="login" element={<Login />}></Route>
-          <Route path="swipe" element={<Swipe />}></Route>
-        </Routes>
+        <Routing />
       </BrowserRouter>
 
     </UserContext.Provider>
   );
 }
+
+
 export default App;
