00cca7b027e6711967f410acbd3d9c239b36523e
nisi0929 <kengo279@icloud.com>
2022-08-05 00:31:15 +0900

    swipe少し修正とapiレスポンス途中まで

--- frontend/src/components/layouts/Menu.jsx
+++ frontend/src/components/layouts/Menu.jsx
@@ -4,8 +4,20 @@ import fish from '../../assets/images/fish.svg';
 import other from '../../assets/images/other.svg';
 import vege from '../../assets/images/vege.svg';
 import { useNavigate } from 'react-router-dom';
+import { recipeList } from '../../lib/api';
+import { useEffect, useState } from 'react';
 export const Menu = ({ hover }) => {
   const navigate = useNavigate();
+  const postSwipeRecipe = (postId) => {
+    const datas = recipeList({
+      tag: postId,
+      id: 0,
+    }).then(function (response) {
+      return response.data;
+    });
+    return datas;
+  };
+  const [items, setItems] = useState(postSwipeRecipe(0));
 
   return (
     <div
@@ -14,28 +26,40 @@ export const Menu = ({ hover }) => {
       <div>
         <div className="grid sm:grid-cols-4 grid-cols-1">
           <button
-            onClick={() => navigate('/swipe')}
+            onClick={() => {
+              postSwipeRecipe(0);
+              navigate('/swipe', { state: 'a' });
+            }}
             className=" flex items-center justify-center hover:opacity-60"
           >
             <img src={meat} alt="" className="w-8 h-8" />
             <p className="mx-2">肉料理</p>
           </button>
           <button
-            onClick={() => navigate('/swipe')}
+            onClick={() => {
+              navigate('/swipe');
+              postSwipeRecipe(2);
+            }}
             className=" flex items-center justify-center hover:opacity-60"
           >
             <img src={fish} alt="" className="w-8 h-8" />
             <p className="mx-2">魚料理</p>
           </button>
           <button
-            onClick={() => navigate('/swipe')}
+            onClick={() => {
+              navigate('/swipe');
+              postSwipeRecipe(3);
+            }}
             className=" flex items-center justify-center hover:opacity-60"
           >
             <img src={vege} alt="" className="w-8 h-8" />
             <p className="mx-2">野菜料理</p>
           </button>
           <button
-            onClick={() => navigate('/swipe')}
+            onClick={() => {
+              navigate('/swipe');
+              postSwipeRecipe(1);
+            }}
             className=" flex items-center justify-center hover:opacity-60"
           >
             <img src={other} alt="" className="w-8 h-8" />
